b71934adf973124c14fd4d6c863007ba8b645b24
MohamedDiaa <mohameddiaa1988@hotmail.com>
2017-01-26 18:25:57 +0100

    Initial Commit

---
+++ TwitchiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitchiOSApp
+//
+//  Created by MohamedDiaa on 1/26/17.
+//  Copyright © 2017 Greenly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
