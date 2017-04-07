55514db90ba156732a95cb9a7446cdb63081782c
MohamedDiaa <mohameddiaa1988@hotmail.com>
2017-04-07 13:19:52 +0200

    removed all pods

--- Pods/Target Support Files/Pods-TwitchiOSApp/Pods-TwitchiOSApp-frameworks.sh
+++ Pods/Target Support Files/Pods-TwitchiOSApp/Pods-TwitchiOSApp-frameworks.sh
@@ -82,20 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/RWPickFlavor/RWPickFlavor.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SKTagView/SKTagView.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SharedWOK/SharedWOK.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/RWPickFlavor/RWPickFlavor.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SKTagView/SKTagView.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SharedWOK/SharedWOK.framework"
-fi
